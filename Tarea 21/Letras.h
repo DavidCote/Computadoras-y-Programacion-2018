9a496816ac9d3146ec768111499363d347c7de27
DavidCote <31465977+DavidCote@users.noreply.github.com>
2017-11-10 20:08:32 -0600

    Add files via upload

---
+++ Tarea 21/Letras.h
@@ -0,0 +1,32 @@
+void letraA(){
+	printf(" AA \n");
+	printf("A  A\n");
+	printf("AAAA\n");
+	printf("A  A\n");
+	printf("A  A\n");
+}
+
+void letraD(){
+	printf("DDD \n");
+	printf("D  D\n");
+	printf("D  D\n");
+	printf("D  D\n");
+	printf("DDD \n");
+}
+
+void letraI(){
+	printf("IIII\n");
+	printf(" II \n");
+	printf(" II \n");
+	printf(" II \n");
+	printf("IIII\n");
+}
+
+void letraV(){
+	printf("V  V\n");
+	printf("V  V\n");
+	printf("V  V\n");
+	printf("V  V\n");
+	printf(" VV \n");
+	
+}
---
+++ Tarea 21/Nombre con funciones.c
@@ -0,0 +1,24 @@
+/*
+David Cote Soriano 
+computadoras y programacion
+programa en C Nombre con funciones
+Tarea No 21
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include "Letras.h"
+
+int main(int argc, char *argv[]) {
+	
+	letraD();
+	printf("\n");
+	letraA();
+	printf("\n");
+	letraV();
+	printf("\n");
+	letraI();
+	printf("\n");
+	letraD();
+	
+	return 0;
+}
